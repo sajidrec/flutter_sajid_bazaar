c9c84bfd2ff9d85c3f158a807aaf9d7733e695dc
MD. Sajid Hossain <sajidrec@gmail.com>
2024-12-28 20:21:56 +0600

    - Search result page dummy design

--- lib/presentation/pages/search_result_page/search_result_page.dart
+++ lib/presentation/pages/search_result_page/search_result_page.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:sajid_bazaar/utils/app_colors_util.dart';
 
 class SearchResultPage extends StatelessWidget {
   const SearchResultPage({
@@ -12,8 +14,49 @@ class SearchResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Text("Search Keyword $searchedKeyWord"),
+        appBar: AppBar(
+          backgroundColor: AppColorsUtil.backgroundGreen,
+          foregroundColor: AppColorsUtil.foreGroundWhite,
+          centerTitle: true,
+          title: Text(searchedKeyWord),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Number of columns
+              crossAxisSpacing: 3,
+              mainAxisSpacing: 3,
+            ),
+            itemCount: 20, // Total number of items
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: AppColorsUtil.backgroundLightGreen,
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: CachedNetworkImage(
+                        fit: BoxFit.cover,
+                        imageUrl:
+                            "https://mwqlqdcwkbcwwoijdkjw.supabase.co/storage/v1/object/public/bazaar_items/reccomended_items/apple.jpg",
+                      ),
+                    ),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Dummy name"),
+                        Text("Price : 100"),
+                        Text("Stocks : 100"),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
