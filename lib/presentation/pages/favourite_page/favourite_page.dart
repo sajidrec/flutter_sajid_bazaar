24e8d84ad48b1803dc6ff157edd558fc3391ce9f
MD. Sajid Hossain <sajidrec@gmail.com>
2025-01-12 14:47:35 +0600

    - Add favourite page dummy design

--- lib/presentation/pages/favourite_page/favourite_page.dart
+++ lib/presentation/pages/favourite_page/favourite_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FavouritePage extends StatelessWidget {
@@ -5,9 +6,30 @@ class FavouritePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Text("Favourite Page"),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 5,
+            horizontal: 5,
+          ),
+          child: ListView.separated(
+            itemBuilder: (context, index) => InkWell(
+              onTap: () {},
+              child: ListTile(
+                leading: CachedNetworkImage(
+                  imageUrl:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJLtBmMcXbiM8k1bNd48Fu08N6XQyjvBi7WQ&s",
+                  // height: Get.height / 8,
+                ),
+                trailing: const Text("\$1000"),
+                title: const Text("Sajid"),
+              ),
+            ),
+            separatorBuilder: (context, index) => const SizedBox(height: 5),
+            itemCount: 20,
+          ),
+        ),
       ),
     );
   }
